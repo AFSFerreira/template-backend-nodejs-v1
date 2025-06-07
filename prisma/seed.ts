9fb54b2c8a957673cc082eaf22b1622fdd10e0fb
Allber Ferreira <alferreira@id.uff.br>
2025-06-07 17:38:19 -0300

    feat: criação parcial do arquivo seed dos modelos

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,38 +1,74 @@
-import { PrismaClient } from '@prisma/client'
-import { hash } from 'bcryptjs'
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+import { env } from '../src/env';
+import { addYears, format } from 'date-fns';
+import { DATE_FORMATS } from '../src/constants/dateFormats';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.upsert({
-    where: { email: "email@example.com" },
-    update: {},
-    create: {
-      cpf: "12345678901",
-      username: "adminuser",
-      name: "Admin User",
-      birthDate: new Date('1990-01-01'),
-      profileImagePath: "uploads/profile-images/dummy-profile-pic.png",
-      curriculumPath: "uploads/curriculums/dummy-curriculum.txt",
-      email: "email@example.com",
-      passwordDigest: await hash("12345678", 10),
-      universityName: "Example University",
-      departmentName: "Astrobiology",
-      institutionComplement: "Research Center",
-      address: "123 Main St, City, Country",
-      // Optional fields
-      // lastLogin: null,
-      // departmentRoleId: null,
-    }
-  })
+  await prisma.user.create({
+    data: {
+      name: 'Admin',
+      username: 'admin.admin',
+      birthDate: new Date(),
+      profileImagePath: '/src/uploads/profile-images/default-profile-pic.png',
+      lattesCVLink: 'http://lattes.cnpq.br/1234567890',
+      profileGSLink: 'https://scholar.google.com/admin.admin',
+      profileRIDLink: null,
+      orcidNumber: '0000-0001-2345-6789',
+      membershipStatus: 'APPROVED',
+      userRole: 'ADMIN',
+      institutionName: 'Universidade Federal da Lua',
+      departmentName: 'Departamento de Astrobiologia',
+      institutionComplement: 'Laboratório de Vida Extraterrestre',
+      occupation: 'RESEARCHER',
+      educationLevel: 'DOCTORATE',
+      publicEmail: true,
+      astrobiologyOrRelatedStartYear: 2010,
+      interestDescription: 'Participo da comunidade por interesse em origens da vida e exoplanetas.',
+      receiveReports: true,
+      email: 'admin.admin@ufl.br',
+      passwordDigest: await hash('123456789', env.HASH_NUMBER_TIMES),
+      loginAttempts: 0,
+      lastLogin: null,
+      activityArea: {
+        create: {
+          mainActivity: 'Astrobiologia Geral',
+        },
+      },
+      address: {
+        create: {
+          houseNumber: "111",
+          street: 'Rua das Galáxias',
+          cityName: 'Cosmópolis',
+          stateName: 'Universo',
+          countryName: 'BR',
+        },
+      },
+      identityDocument: {
+        create: {
+          cpf: '123.456.789-00',
+        },
+      },
+      enrolledCourse: {
+        create: {
+          courseName: 'Introdução à Astrobiologia',
+          startGraduationDate: format(new Date(), DATE_FORMATS.MONTH_YEAR),
+          expectedGraduationDate: format(addYears(new Date(), 4), DATE_FORMATS.MONTH_YEAR),
+          scholarshipHolder: true,
+          sponsoringOrganization: 'Universidade Lunar',
+          supervisorName: 'Neew Armstrong'
+        },
+      },
+    },
+  });
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
+  .then(async () => { await prisma.$disconnect(); })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error('Erro ao executar seed:', e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
