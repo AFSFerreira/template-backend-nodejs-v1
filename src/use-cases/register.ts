c2f2d70c70d0466cabca9fdd0f4d18e66812d661
Allber Ferreira <alferreira@id.uff.br>
2025-06-07 04:14:13 -0300

    chore: mudanças estruturais temporárias do linter

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,41 +1,41 @@
-import type { UsersRepository } from '../repositories/users-repository'
-import { hash } from 'bcryptjs'
-import type { User } from '@prisma/client'
-import { UserWithSameEmailError } from './errors/user-with-same-email-error'
+// import type { UsersRepository } from '../repositories/users-repository'
+// import { hash } from 'bcryptjs'
+// import type { User } from '@prisma/client'
+// import { UserWithSameEmailError } from './errors/user-with-same-email-error'
 
-interface RegisterUseCaseRequest {
-  name: string
-  email: string
-  password: string
-}
+// interface RegisterUseCaseRequest {
+//   name: string
+//   email: string
+//   password: string
+// }
 
-interface RegisterUseCaseResponse {
-  user: User
-}
+// interface RegisterUseCaseResponse {
+//   user: User
+// }
 
-export class RegisterUseCase {
-  constructor(private readonly usersRepository: UsersRepository) {}
+// export class RegisterUseCase {
+//   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async execute({
-    name,
-    email,
-    password,
-  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
-    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+//   async execute({
+//     name,
+//     email,
+//     password,
+//   }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+//     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-    if (userWithSameEmail !== null) {
-      throw new UserWithSameEmailError()
-    }
-    const passwordDigest = await hash(password, 10)
+//     if (userWithSameEmail !== null) {
+//       throw new UserWithSameEmailError()
+//     }
+//     const passwordDigest = await hash(password, 10)
 
-    const user = await this.usersRepository.create({
-      name,
-      email,
-      password_digest: passwordDigest,
-    })
+//     const user = await this.usersRepository.create({
+//       name,
+//       email,
+//       password_digest: passwordDigest,
+//     })
 
-    return {
-      user,
-    }
-  }
-}
+//     return {
+//       user,
+//     }
+//   }
+// }
