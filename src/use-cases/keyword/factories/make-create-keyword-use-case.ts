3721eff4d26f487ec510a3395f9227b39f297586
Allber Ferreira <alferreira@id.uff.br>
2025-06-26 00:54:39 -0300

    fix: remoção de imagem obsoleta do backend

--- src/use-cases/keyword/factories/make-create-keyword-use-case.ts
+++ src/use-cases/keyword/factories/make-create-keyword-use-case.ts
@@ -1,7 +1,6 @@
 import { PrismaKeywordRepository } from '@/repositories/prisma/prisma-keyword-repository'
 import { CreateKeywordUseCase } from '../create-keyword-use-case'
 
-
 export function makeCreateKeywordUseCase() {
   const keywordRepository = new PrismaKeywordRepository()
   return new CreateKeywordUseCase(keywordRepository)
