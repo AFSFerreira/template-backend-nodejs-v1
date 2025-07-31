e419d9f3e5679d5ffaccdc03812633b3d15d589d
Allber Ferreira <alferreira@id.uff.br>
2025-07-31 19:09:23 -0300

    refactor: movendo arquivo de flatten user para a pasta de services

--- src/services/export-users.ts
+++ src/services/export-users.ts
@@ -1,6 +1,6 @@
 import type { UserWithDetails } from '@/@types/user-with-details'
+import { flattenUser } from '@/services/flatten-user'
 import { toCsv } from '@/utils/csv'
-import { flattenUser } from '@/utils/flatten-user'
 
 export function exportUsersAsCsv(users: UserWithDetails[]): string {
   const flattened = users.map(flattenUser)
