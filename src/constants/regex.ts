c32c0fa121e66cc14129b3060279a7ffb8384ce1
Allber Ferreira <alferreira@id.uff.br>
2025-08-06 21:06:49 -0300

    feat: habilitando pgtgrm para consultas de conteúdos de blogs

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,3 +2,4 @@ export const YEAR_MONTH_REGEX = /^\d{4}-(0[1-9]|1[0-2])$/
 export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).+$/
 export const INNER_SPACES_REGEX = /^\S+$/
 export const ORCID_NUMBER_VALIDATION_REGEX = /^(\d{4}-){3}\d{3}[\dX]$/
+export const SQL_IDENTIFIER_REGEX = /^[a-zA-Z_][a-zA-Z0-9_]*$/
