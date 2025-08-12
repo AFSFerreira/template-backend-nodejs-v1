d35ffc4ac34a706bb2f5ce83ff29f5a866c8a7e6
Allber Ferreira <alferreira@id.uff.br>
2025-08-12 09:14:34 -0300

    fix: remoção de arquivo obsoleto da criação de palavras chave

--- src/use-cases/factories/keyword/make-create-user-keyword-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaKeywordRepository } from '@/repositories/prisma/prisma-keyword-repository'
-import { CreateUserKeywordUseCase } from '@/use-cases/keyword/create-user-keyword-use-case'
-
-export function makeCreateUserKeywordUseCase() {
-  const keywordRepository = new PrismaKeywordRepository()
-  const createKeywordUseCase = new CreateUserKeywordUseCase(keywordRepository)
-
-  return createKeywordUseCase
-}
