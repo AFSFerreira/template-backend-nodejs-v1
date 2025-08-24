9885dd58df0f36da015bd26243b57c021d887ed4
Allber Ferreira <alferreira@id.uff.br>
2025-08-24 08:47:31 -0300

    feat: criação de função de tradução dos níveis de escolaridade

---
+++ src/services/translate-education-level.ts
@@ -0,0 +1,53 @@
+import { EducationLevelType } from "@prisma/client"
+
+type TranslationLanguageType = 'en' | 'pt'
+
+function translateEducationalLevelPt(educationLevel: EducationLevelType) {
+    switch (educationLevel) {
+        case EducationLevelType.ELEMENTARY_SCHOOL:
+            return "ENSINO FUNDAMENTAL"
+        case EducationLevelType.HIGH_SCHOOL:
+            return "ENSINO MÉDIO"
+        case EducationLevelType.UNDERGRADUATE_STUDENT:
+            return "ALUNO DE GRADUAÇÃO"
+        case EducationLevelType.BACHELOR:
+            return "BACHAREL"
+        case EducationLevelType.DOCTORATE_STUDENT:
+            return "ALUNO DE DOUTORADO"
+        case EducationLevelType.DOCTORATE:
+            return "DOUTORADO"
+        case EducationLevelType.MASTER_STUDENT:
+            return "ALUNO DE MESTRADO"
+        case EducationLevelType.MASTER:
+            return "MESTRADO"
+        case EducationLevelType.OTHER:
+            return "OUTRO"
+    }
+}
+
+function translateEducationalLevelEn(educationLevel: EducationLevelType) {
+    switch (educationLevel) {
+        case EducationLevelType.ELEMENTARY_SCHOOL:
+            return "ELEMENTARY SCHOOL"
+        case EducationLevelType.HIGH_SCHOOL:
+            return "HIGH SCHOOL"
+        case EducationLevelType.UNDERGRADUATE_STUDENT:
+            return "UNDERGRADUATE STUDENT"
+        case EducationLevelType.BACHELOR:
+            return "BACHELOR"
+        case EducationLevelType.DOCTORATE_STUDENT:
+            return "DOCTORATE STUDENT"
+        case EducationLevelType.DOCTORATE:
+            return "DOCTORATE"
+        case EducationLevelType.MASTER_STUDENT:
+            return "MASTER STUDENT"
+        case EducationLevelType.MASTER:
+            return "MASTER"
+        case EducationLevelType.OTHER:
+            return "OTHER"
+    }
+}
+
+export function getTranslatedEducationLevels(lang: TranslationLanguageType) {
+    return Object.values(EducationLevelType).map(lang === 'en' ? translateEducationalLevelEn : translateEducationalLevelPt)
+}
