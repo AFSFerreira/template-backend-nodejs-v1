d794026f010199a7640e7b21e147ec80595f4d94
Rian Polonini <rianbrenopolonini@gmail.com>
2025-09-16 20:02:55 -0300

    feat: adiciona rota de health check com verificação de status do sistema

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { activityAreaRoutes } from './controllers/activity-area/routes'
+import { healthCheck } from './controllers/health-check/health-check.controller'
 import { userRoutes } from './controllers/user/routes'
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(userRoutes, { prefix: '/users' })
   app.register(activityAreaRoutes, { prefix: '/activity-area' })
+  app.get('/health', healthCheck)
 }
