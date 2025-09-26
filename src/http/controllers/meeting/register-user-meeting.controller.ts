1b74487bb62316368ff09ecc80b78c9fed1a0660
AFSFerreira <alferreira@id.uff.br>
2025-09-26 15:35:39 -0300

    refactor: simplify register user meeting controller and remove unused presenter fields

--- src/http/controllers/meeting/register-user-meeting.controller.ts
+++ src/http/controllers/meeting/register-user-meeting.controller.ts
@@ -1,31 +1,20 @@
-import { MeetingParticipationPresenter } from '@presenters/meeting-participation-presenter'
 import { registerUserMeetingBodySchema } from '@schemas/meeting/register-user-meeting-body-schema'
 import { registerUserMeetingParamsSchema } from '@schemas/meeting/register-user-meeting-params-schema'
 import { makeRegisterUserMeetingUseCase } from '@use-cases/factories/meeting/make-register-user-meeting-use-case'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
-interface RegisterUserMeetingParams {
-  meetingId: string
-}
-
-interface RegisterUserMeetingRequest extends FastifyRequest<{
-  Params: RegisterUserMeetingParams
-}> {}
-
-export async function registerUserMeeting(request: RegisterUserMeetingRequest, reply: FastifyReply) {
+export async function registerUserMeeting(request: FastifyRequest, reply: FastifyReply) {
   const { meetingId } = registerUserMeetingParamsSchema.parse(request.params)
   const { sub: userId } = request.user
   const parsedBody = registerUserMeetingBodySchema.parse(request.body)
 
   const registerUserMeetingUseCase = makeRegisterUserMeetingUseCase()
 
-  const { meetingParticipation } = await registerUserMeetingUseCase.execute({
+  await registerUserMeetingUseCase.execute({
     ...parsedBody,
     userId,
     meetingId,
   })
 
-  return await reply.status(201).send({
-    data: MeetingParticipationPresenter.toHTTP(meetingParticipation),
-  })
+  return await reply.status(201).send()
 }
--- src/http/presenters/meeting-participation-presenter.ts
+++ src/http/presenters/meeting-participation-presenter.ts
@@ -1,23 +1,22 @@
 import type { MeetingParticipation } from '@prisma/client'
 
 interface HTTPMeetingParticipation {
-  id: number
   createdAt: Date
   userId: number | null
   guestId: number | null
   meetingId: number
 }
 
+// TODO: Verificar posteriormente se este presenter é necessário e, se não for, removê-lo
 export class MeetingParticipationPresenter {
   static toHTTP(meetingParticipation: MeetingParticipation): HTTPMeetingParticipation
-  static toHTTP(meetingParticipations: MeetingParticipation[]): HTTPMeetingParticipation[]
+  static toHTTP(meetingParticipants: MeetingParticipation[]): HTTPMeetingParticipation[]
   static toHTTP(input: MeetingParticipation | MeetingParticipation[]): HTTPMeetingParticipation | HTTPMeetingParticipation[] {
     if (Array.isArray(input)) {
       return input.map((participation) => MeetingParticipationPresenter.toHTTP(participation))
     }
 
     return {
-      id: input.id,
       createdAt: input.createdAt,
       userId: input.userId,
       guestId: input.guestId,
