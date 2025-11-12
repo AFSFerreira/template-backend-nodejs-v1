6b5ea243f3f01cfe57c5e28f539932a4387ed1db
Allber Fellype Soares Ferreira <alferreira@id.uff.br>
2025-11-12 20:39:17 -0300

    Feat/log

--- src/services/register-blog-views.ts
+++ src/services/register-blog-views.ts
@@ -1,4 +1,5 @@
 import { BLOG_IP_VIEW_TTL } from '@constants/redis-configuration-constants'
+import { logger } from '@lib/logger'
 import type Redis from 'ioredis'
 
 interface IRegisterBlogViews {
@@ -8,6 +9,8 @@ interface IRegisterBlogViews {
 }
 
 export async function registerBlogViews({ blogId, ip, redis }: IRegisterBlogViews) {
+  logger.info({ blogId, ip }, 'Cached blog visualization')
+
   const key = `view:blog:${blogId}:userIp:${ip}`
   const blogWasNotRecentlyViewed: 'OK' | null = await redis.set(key, '1', 'PX', BLOG_IP_VIEW_TTL, 'NX')
 
