4a8f1c944d884702de82f04a400cfbb90298fc76
Allber Fellype Soares Ferreira <alferreira@id.uff.br>
2025-11-12 20:52:45 -0300

    Fix/ip user

--- src/services/register-blog-views.ts
+++ src/services/register-blog-views.ts
@@ -9,10 +9,11 @@ interface IRegisterBlogViews {
 }
 
 export async function registerBlogViews({ blogId, ip, redis }: IRegisterBlogViews) {
-  logger.info({ blogId, ip }, 'Cached blog visualization')
 
   const key = `view:blog:${blogId}:userIp:${ip}`
   const blogWasNotRecentlyViewed: 'OK' | null = await redis.set(key, '1', 'PX', BLOG_IP_VIEW_TTL, 'NX')
+
+  logger.info({ blogId, ip, blogWasNotRecentlyViewed }, 'Cached blog visualization')
 
   return blogWasNotRecentlyViewed
 }
