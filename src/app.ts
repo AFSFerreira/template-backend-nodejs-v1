647a38530eceebbb2bcd87a232ab68ff56984ad7
AFSFerreira <alferreira@id.uff.br>
2025-12-06 02:06:25 -0300

    feat: integrate tsyringe for dependency injection and update configurations

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,8 @@ import { logResponse } from './http/plugins/response-logger'
 import { preSerialization } from './http/plugins/serializer'
 import { staticFileRoutes } from './http/plugins/static-files'
 import { appRoutes } from './http/routes'
+import "reflect-metadata"
+import '@lib/tsyringe/index'
 
 export const app = fastify(fastifyConfig)
 
