0feed97fff817816ceca9adac3d5f94c38d81915
AFSFerreira <alferreira@id.uff.br>
2026-01-15 17:14:41 -0300

    fix: update user registration types to use correct education schemas

--- src/@types/http/schemas/user/register-body-schema.ts
+++ src/@types/http/schemas/user/register-body-schema.ts
@@ -1,8 +1,7 @@
-import type { RegisterUserHighLevelEducationType } from '../../../schemas/utils/register-user-high-level-education-type'
-import type { RegisterUserLowLevelEducationType } from '../../../schemas/utils/register-user-low-level-education-type'
 import type { RegisterUserHighLevelStudentEducationType } from './register-user-high-level-student-education-type'
+import type { RegisterUserLowLevelEducationType } from './register-user-low-level-education-type'
 
 export type RegisterUserBodySchemaType =
   | RegisterUserLowLevelEducationType
   | RegisterUserHighLevelStudentEducationType
-  | RegisterUserHighLevelEducationType
+  | RegisterUserHighLevelStudentEducationType
--- src/services/guards/is-register-user-high-level-education.ts
+++ src/services/guards/is-register-user-high-level-education.ts
@@ -1,9 +1,9 @@
-import type { RegisterUserHighLevelEducationType } from '@custom-types/schemas/utils/register-user-high-level-education-type'
 import type { HasEducationLevel } from '@custom-types/services/guards/has-education-level'
 import { HIGH_LEVEL_EDUCATION_TYPE_SET } from '@constants/sets'
+import type { RegisterUserHighLevelStudentEducationType } from '@custom-types/http/schemas/user/register-user-high-level-student-education-type'
 
 export function isRegisterUserHighLevelEducation<T extends HasEducationLevel>(
   input: T,
-): input is T & RegisterUserHighLevelEducationType {
+): input is T & RegisterUserHighLevelStudentEducationType {
   return HIGH_LEVEL_EDUCATION_TYPE_SET.has(input.user.educationLevel)
 }
