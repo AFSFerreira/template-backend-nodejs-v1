e36adcafd0ecee33d41e7a7b8ad98b15fe046bb4
AFSFerreira <alferreira@id.uff.br>
2026-01-21 19:13:15 -0300

    chore: add migration script to drop comment_likes and comments tables with warnings

---
+++ prisma/migrations/20260121220747_dev/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `comment_likes` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `comments` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- AlterEnum
+
+
+
+
+
+
