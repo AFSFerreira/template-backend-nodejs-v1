3a577a345c697dd78907adf600c49828cf4e1a7a
AFSFerreira <alferreira@id.uff.br>
2026-01-23 21:46:15 -0300

    feat: add pixSchema for validating PIX data using pixkey

---
+++ src/http/schemas/utils/generic-components/pix-schema.ts
@@ -0,0 +1,4 @@
+import { validate as validatePix } from 'pixkey'
+import { limitedNonemptyTextSchema } from '../primitives/limited-nonempty-text-schema'
+
+export const pixSchema = limitedNonemptyTextSchema.refine((data) => validatePix(data))
