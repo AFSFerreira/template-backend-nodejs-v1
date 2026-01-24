864d50e54a3c0f4ed3dd57b77b82ba64b82b5386
AFSFerreira <alferreira@id.uff.br>
2026-01-24 03:38:45 -0300

    refactor: streamline UserDetailedPresenter's toHTTP method for improved readability

--- src/http/presenters/user/user-detailed.presenter.ts
+++ src/http/presenters/user/user-detailed.presenter.ts
@@ -4,84 +4,83 @@ import { truncateDate } from '@utils/formatters/truncate-date'
 
 export class UserDetailedPresenter implements IPresenterStrategy<UserDetailedPresenterInput, HTTPUserWithDetails> {
   public toHTTP(input: UserDetailedPresenterInput): HTTPUserWithDetails {
-        return {
-          id: input.publicId,
-          astrobiologyOrRelatedStartYear: input.astrobiologyOrRelatedStartYear,
-          departmentName: input.departmentName,
-          role: input.role,
-          educationLevel: input.educationLevel,
-          email: input.email,
-          emailIsPublic: input.emailIsPublic,
-          fullName: input.fullName,
-          identityDocument: input.identityDocument,
-          identityType: input.identityType,
-          institutionComplement: input.institutionComplement,
-          linkGoogleScholar: input.linkGoogleScholar,
-          linkLattes: input.linkLattes,
-          linkResearcherId: input.linkResearcherId,
-          occupation: input.occupation,
-          orcidNumber: input.orcidNumber,
-          publicInformation: input.publicInformation,
-          receiveReports: input.receiveReports,
-          username: input.username,
-          profileImage: input.profileImage,
-          birthdate: truncateDate(input.birthdate),
+    return {
+      id: input.publicId,
+      astrobiologyOrRelatedStartYear: input.astrobiologyOrRelatedStartYear,
+      departmentName: input.departmentName,
+      role: input.role,
+      educationLevel: input.educationLevel,
+      email: input.email,
+      emailIsPublic: input.emailIsPublic,
+      fullName: input.fullName,
+      identityDocument: input.identityDocument,
+      identityType: input.identityType,
+      institutionComplement: input.institutionComplement,
+      linkGoogleScholar: input.linkGoogleScholar,
+      linkLattes: input.linkLattes,
+      linkResearcherId: input.linkResearcherId,
+      occupation: input.occupation,
+      orcidNumber: input.orcidNumber,
+      publicInformation: input.publicInformation,
+      receiveReports: input.receiveReports,
+      username: input.username,
+      profileImage: input.profileImage,
+      birthdate: truncateDate(input.birthdate),
 
-          institutionName: input.Institution?.name,
+      institutionName: input.Institution?.name,
 
-          keywords:
-            input.Keyword && input.Keyword.length > 0 ? input.Keyword?.map((keyword) => keyword.value) : undefined,
+      keywords: input.Keyword && input.Keyword.length > 0 ? input.Keyword?.map((keyword) => keyword.value) : undefined,
 
-          mainActivityArea: input.ActivityArea?.area,
-          subActivityArea: input.SubActivityArea?.area,
+      mainActivityArea: input.ActivityArea?.area,
+      subActivityArea: input.SubActivityArea?.area,
 
-          address: input.Address
-            ? {
-                zip: input.Address.zip,
-                number: input.Address.number,
-                district: input.Address.district,
-                street: input.Address.street,
-                city: input.Address.city,
-                country: input.Address.State.Country.name,
-                state: input.Address.State.name,
-                complement: input.Address.complement,
-              }
-            : undefined,
+      address: input.Address
+        ? {
+            zip: input.Address.zip,
+            number: input.Address.number,
+            district: input.Address.district,
+            street: input.Address.street,
+            city: input.Address.city,
+            country: input.Address.State.Country.name,
+            state: input.Address.State.name,
+            complement: input.Address.complement,
+          }
+        : undefined,
 
-          enrolledCourse: input.EnrolledCourse
-            ? {
-                courseName: input.EnrolledCourse.courseName,
-                startGraduationDate: truncateDate(input.EnrolledCourse.startGraduationDate),
-                expectedGraduationDate: truncateDate(input.EnrolledCourse.expectedGraduationDate),
-                supervisorName: input.EnrolledCourse.supervisorName,
-                scholarshipHolder: input.EnrolledCourse.scholarshipHolder,
-                sponsoringOrganization: input.EnrolledCourse.sponsoringOrganization,
-              }
-            : undefined,
+      enrolledCourse: input.EnrolledCourse
+        ? {
+            courseName: input.EnrolledCourse.courseName,
+            startGraduationDate: truncateDate(input.EnrolledCourse.startGraduationDate),
+            expectedGraduationDate: truncateDate(input.EnrolledCourse.expectedGraduationDate),
+            supervisorName: input.EnrolledCourse.supervisorName,
+            scholarshipHolder: input.EnrolledCourse.scholarshipHolder,
+            sponsoringOrganization: input.EnrolledCourse.sponsoringOrganization,
+          }
+        : undefined,
 
-          academicPublications:
-            input.AcademicPublication && input.AcademicPublication.length > 0
-              ? input.AcademicPublication.map((academicPublication) => ({
-                  title: academicPublication.title,
-                  authors: academicPublication.AcademicPublicationAuthors?.map((author) => author.name),
-                  publicationYear: academicPublication.publicationYear,
-                  journalName: academicPublication.journalName,
-                  volume: academicPublication.volume,
-                  editionNumber: academicPublication.editionNumber,
-                  startPage: academicPublication.startPage,
-                  linkDoi: academicPublication.linkDoi,
-                  area: academicPublication.ActivityArea?.area,
-                }))
-              : undefined,
+      academicPublications:
+        input.AcademicPublication && input.AcademicPublication.length > 0
+          ? input.AcademicPublication.map((academicPublication) => ({
+              title: academicPublication.title,
+              authors: academicPublication.AcademicPublicationAuthors?.map((author) => author.name),
+              publicationYear: academicPublication.publicationYear,
+              journalName: academicPublication.journalName,
+              volume: academicPublication.volume,
+              editionNumber: academicPublication.editionNumber,
+              startPage: academicPublication.startPage,
+              linkDoi: academicPublication.linkDoi,
+              area: academicPublication.ActivityArea?.area,
+            }))
+          : undefined,
 
-          directorBoardInfo: input.DirectorBoard
-            ? {
-                linkLattes: input.linkLattes,
-                name: input.fullName,
-                profileImage: input.DirectorBoard.profileImage ?? input.profileImage,
-                position: input.DirectorBoard.DirectorPosition.position,
-              }
-            : undefined,
-        }
+      directorBoardInfo: input.DirectorBoard
+        ? {
+            linkLattes: input.linkLattes,
+            name: input.fullName,
+            profileImage: input.DirectorBoard.profileImage ?? input.profileImage,
+            position: input.DirectorBoard.DirectorPosition.position,
+          }
+        : undefined,
+    }
   }
 }
