9f6d147c9490d54ea21c836294ab6bbc9360f401
AFSFerreira <alferreira@id.uff.br>
2026-01-27 23:41:59 -0300

    fix: update app names and structure in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'astrobiologia-backend',
+      name: 'astrobiologia-staging',
       script: './dist/server.js',
       cwd: '/home/injunior-infra/dev/projects/astrobiologia-backend',
       max_memory_restart: '256M',
@@ -19,7 +19,28 @@ module.exports = {
       // Hardening:
       restart_delay: 4000,
       min_uptime: '5s',
-      max_restarts: 10
-    }
-  ]
+      max_restarts: 10,
+    },
+    {
+      name: 'astrobiologia',
+      script: './dist/server.js',
+      cwd: '/home/astrobiologia-infra/infra/astrobio-infra/projects/astrobiologia-backend',
+      max_memory_restart: '512M',
+
+      // Node flags + Environment:
+      node_args: '--env-file=.env',
+
+      // Logging:
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      merge_logs: true,
+      log_date_format: 'DD-MM-YYYY HH:mm:ss Z',
+      log_type: 'json',
+
+      // Hardening:
+      restart_delay: 4000,
+      min_uptime: '5s',
+      max_restarts: 10,
+    },
+  ],
 }
