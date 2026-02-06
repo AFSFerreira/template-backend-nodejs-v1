0e99bca7ff62f6426e2ca644cbf2a8dbe528d7fd
AFSFerreira <alferreira@id.uff.br>
2026-02-06 02:46:11 -0300

    chore: update dependencies and refactor pix schema validation
    - Updated package.json to replace "pixkey" with "pix-utils" version 2.8.2.
    - Modified pnpm-lock.yaml to reflect the changes in dependencies and removed obsolete entries.
    - Refactored pix-schema.ts to use the new "pix-utils" for parsing and error handling instead of "pixkey".

--- src/http/schemas/utils/generic-components/pix-schema.ts
+++ src/http/schemas/utils/generic-components/pix-schema.ts
@@ -1,4 +1,5 @@
-import { validate as validatePix } from 'pixkey'
+import type { PixError } from 'pix-utils'
+import { parsePix } from 'pix-utils'
 import { limitedNonemptyTextSchema } from '../primitives/limited-nonempty-text-schema'
 
-export const pixSchema = limitedNonemptyTextSchema.refine((data) => validatePix(data).length > 0)
+export const pixSchema = limitedNonemptyTextSchema.refine((data) => (parsePix(data) as PixError).error === undefined)
