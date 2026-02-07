594bdd8cd0eb58886d965b09472421f81fef322b
AFSFerreira <alferreira@id.uff.br>
2026-02-06 23:25:05 -0300

    fix: corrigir importação do esquema de texto não vazio para validação de números inteiros

--- src/http/schemas/utils/generic-components/numeric-integer-string-schema.ts
+++ src/http/schemas/utils/generic-components/numeric-integer-string-schema.ts
@@ -1,5 +1,5 @@
 import { DIGITS_ONLY_REGEX } from '@constants/regex-constants'
 import { NUMERIC_INTEGER_STRING_INVALID } from '@messages/validations/common-validations'
-import { nonemptyTextSchema } from '../primitives/nonempty-text-schema'
+import { limitedNonemptyTextSchema } from '../primitives/limited-nonempty-text-schema'
 
-export const numericIntegerStringSchema = nonemptyTextSchema.regex(DIGITS_ONLY_REGEX, NUMERIC_INTEGER_STRING_INVALID)
+export const numericIntegerStringSchema = limitedNonemptyTextSchema.regex(DIGITS_ONLY_REGEX, NUMERIC_INTEGER_STRING_INVALID)
