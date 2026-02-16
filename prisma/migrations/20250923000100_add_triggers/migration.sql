8d9566acd64a7c9515a326923be80264b30aacb9
Allber Ferreira <allber.ferreira@injunior.com.br>
2026-02-16 01:20:12 -0300

    refactor: atualizar triggers e funções de busca para usuários, blogs e publicações acadêmicas

--- prisma/migrations/20250923000100_add_triggers/migration.sql
+++ prisma/migrations/20250923000100_add_triggers/migration.sql
@@ -1,120 +1,85 @@
-DROP TRIGGER IF EXISTS tsvector_pt_update_users_full_name ON "public"."users";
-DROP FUNCTION IF EXISTS users_full_name_tsv_pt_trigger;
+DROP TRIGGER IF EXISTS users_search_data_trigger ON public.users;
+DROP FUNCTION IF EXISTS users_search_data_trigger;
 
-CREATE OR REPLACE FUNCTION users_full_name_tsv_pt_trigger()
+CREATE OR REPLACE FUNCTION users_search_data_trigger()
 RETURNS trigger AS $$
 BEGIN
-  NEW.full_name_tsv_pt := to_tsvector(
-    'portuguese',
-    unaccent(substring(coalesce(NEW.full_name, '') FROM 1 FOR 255))
-  );
-  RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+  IF TG_OP = 'INSERT' OR
+    NEW.full_name IS DISTINCT FROM OLD.full_name THEN
 
-CREATE TRIGGER tsvector_pt_update_users_full_name
-BEFORE INSERT OR UPDATE ON "public"."users"
-FOR EACH ROW EXECUTE FUNCTION users_full_name_tsv_pt_trigger();
+      NEW.full_name_unaccent := unaccent(substring(coalesce(NEW.full_name, '') FROM 1 FOR 255));
 
---------------------------------------------------------
-
-DROP TRIGGER IF EXISTS tsvector_pt_update_search_blogs ON blogs;
-DROP FUNCTION IF EXISTS blogs_search_tsv_pt_trigger;
-
-CREATE OR REPLACE FUNCTION blogs_search_tsv_pt_trigger()
-RETURNS trigger AS $$
-BEGIN
-  NEW.search_tsv_pt :=
-    setweight(
-      to_tsvector(
+      NEW.full_name_tsv_pt := to_tsvector(
         'portuguese',
-        unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255))
-      ),
-      'A'
-    )
-    || setweight(
-      to_tsvector(
-        'portuguese',
-        unaccent(substring(coalesce(NEW.search_content, '') FROM 1 FOR 511))
-      ),
-      'B'
-    );
+        NEW.full_name_unaccent
+      );
+  END IF;
+
   RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER tsvector_pt_update_search_blogs
-BEFORE INSERT OR UPDATE ON blogs
-FOR EACH ROW EXECUTE FUNCTION blogs_search_tsv_pt_trigger();
-
---------------------------------------------------------
-
-DROP TRIGGER IF EXISTS blogs_title_unaccent_trigger ON blogs;
-DROP FUNCTION IF EXISTS fill_blogs_title_unaccented_columns;
-
-CREATE OR REPLACE FUNCTION fill_blogs_title_unaccented_columns()
-RETURNS trigger AS $$
-BEGIN
-  NEW.title_unaccent := unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255));
-  RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER blogs_title_unaccent_trigger
-BEFORE INSERT OR UPDATE ON "public"."blogs"
-FOR EACH ROW EXECUTE FUNCTION fill_blogs_title_unaccented_columns();
-
---------------------------------------------------------
-
-DROP TRIGGER IF EXISTS users_full_name_unaccent_trigger ON public.users;
-DROP FUNCTION IF EXISTS fill_users_full_name_unaccent_trigger;
-
-CREATE FUNCTION fill_users_full_name_unaccent_trigger()
-RETURNS trigger AS $$
-BEGIN
-  NEW.full_name_unaccent := unaccent(substring(coalesce(NEW.full_name, '') FROM 1 FOR 255));
-  RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER users_full_name_unaccent_trigger
+CREATE TRIGGER users_search_data_trigger
 BEFORE INSERT OR UPDATE ON public.users
-FOR EACH ROW EXECUTE FUNCTION fill_users_full_name_unaccent_trigger();
+FOR EACH ROW EXECUTE FUNCTION users_search_data_trigger();
 
 --------------------------------------------------------
 
-DROP TRIGGER IF EXISTS academic_publications_title_unaccent_trigger ON public.academic_publications;
-DROP FUNCTION IF EXISTS fill_academic_publications_title_unaccented_columns;
+DROP TRIGGER IF EXISTS blogs_search_data_trigger ON public.blogs;
+DROP FUNCTION IF EXISTS blogs_search_data_trigger;
 
-CREATE OR REPLACE FUNCTION fill_academic_publications_title_unaccented_columns()
+CREATE OR REPLACE FUNCTION blogs_search_data_trigger()
 RETURNS trigger AS $$
 BEGIN
-  NEW.title_unaccent := unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255));
+  IF TG_OP = 'INSERT' OR
+    NEW.title IS DISTINCT FROM OLD.title OR
+    NEW.search_content IS DISTINCT FROM OLD.search_content THEN
+
+      NEW.title_unaccent := unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255));
+
+      NEW.search_tsv_pt :=
+        setweight(
+          to_tsvector('portuguese', NEW.title_unaccent),
+          'A'
+        )
+        || setweight(
+          to_tsvector('portuguese', unaccent(substring(coalesce(NEW.search_content, '') FROM 1 FOR 511))),
+          'B'
+        );
+  END IF;
+
   RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER academic_publications_title_unaccent_trigger
-BEFORE INSERT OR UPDATE ON public.academic_publications
-FOR EACH ROW EXECUTE FUNCTION fill_academic_publications_title_unaccented_columns();
+CREATE TRIGGER blogs_search_data_trigger
+BEFORE INSERT OR UPDATE ON public.blogs
+FOR EACH ROW EXECUTE FUNCTION blogs_search_data_trigger();
 
 --------------------------------------------------------
 
-DROP TRIGGER IF EXISTS tsvector_pt_update_academic_publications_title ON public.academic_publications;
-DROP FUNCTION IF EXISTS academic_publications_title_tsv_pt_trigger;
+DROP TRIGGER IF EXISTS academic_publications_search_data_trigger ON public.academic_publications;
+DROP FUNCTION IF EXISTS academic_publications_search_data_trigger;
 
-CREATE OR REPLACE FUNCTION academic_publications_title_tsv_pt_trigger()
+CREATE OR REPLACE FUNCTION academic_publications_search_data_trigger()
 RETURNS trigger AS $$
 BEGIN
-  NEW.title_tsv_pt := to_tsvector(
-    'portuguese',
-    unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255))
-  );
+  IF TG_OP = 'INSERT' OR
+    NEW.title IS DISTINCT FROM OLD.title THEN
+
+      NEW.title_unaccent := unaccent(substring(coalesce(NEW.title, '') FROM 1 FOR 255));
+
+      NEW.title_tsv_pt := to_tsvector(
+        'portuguese',
+        NEW.title_unaccent
+      );
+  END IF;
+
   RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER tsvector_pt_update_academic_publications_title
+CREATE TRIGGER academic_publications_search_data_trigger
 BEFORE INSERT OR UPDATE ON public.academic_publications
-FOR EACH ROW EXECUTE FUNCTION academic_publications_title_tsv_pt_trigger();
+FOR EACH ROW EXECUTE FUNCTION academic_publications_search_data_trigger();
 
