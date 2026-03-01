16ef3490c6191a861bbef048da4b4a81044f6265
Allber Ferreira <alferreira@id.uff.br>
2026-02-28 21:44:09 -0300

    chore: corrigir importação do plugin advancedFormat e do locale do dayjs

--- src/lib/dayjs/index.ts
+++ src/lib/dayjs/index.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
+import advancedFormat from 'dayjs/plugin/advancedFormat.js'
+import 'dayjs/locale/pt-br.js'
 
 dayjs.locale('pt-br')
 dayjs.extend(advancedFormat)
